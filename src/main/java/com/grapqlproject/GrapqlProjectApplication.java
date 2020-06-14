f8fb531e1ad2f1b61ddd65039ef25746a1c0e9cd
Sameer Neema <sameer.neema@moveinsync.com>
2020-06-14 14:54:13 +0530

    initial commit

---
+++ src/main/java/com/grapqlproject/GrapqlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.grapqlproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapqlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrapqlProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grapqlproject/GrapqlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grapqlproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapqlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
